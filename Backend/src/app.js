43f12b37d86ab291557cbf83755d1ab6d9ee860b
sakshiagrawal03 <agrawalsag151@gmail.com>
2026-04-09 16:35:28 +0530

    fixing the frontend url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,37 +6,19 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cookieParser());
 
+// app.use(cors({
+//      origin: process.env.FRONTEND_URL ,
+//      credentials: true
+// }));
 app.use(cors({
-     origin: process.env.FRONTEND_URL ,
-     credentials: true
+    origin: [
+        "http://localhost:5173", 
+        "https://preparation-pilot12.vercel.app" // Added vercel frontend URL
+    ],
+    credentials: true
 }));
 
 
-//deplooyyyyyyyyyyyyy
-// const allowedOrigins = [
-//   "https://prep-pilot-frontend-sigma.vercel.app", // Your Sigma URL
-//   "https://prep-pilot-green-eta.vercel.app",    // Your Green URL
-//   "http://localhost:5173"                       // Your Localhost
-// ];
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     // 1. Allow requests with no origin (like mobile apps or Postman)
-//     if (!origin) return callback(null, true);
-
-//     // 2. Check if the origin is in our allowed list
-//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('CORS blocked: Origin not allowed'));
-//     }
-//   },
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"]
-// }));
-
-/////////
 
 /* Routes */ 
 const authRouter = require('./routes/auth.routes');
