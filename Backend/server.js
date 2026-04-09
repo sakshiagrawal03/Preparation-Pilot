026555fb95282d74ffb8faeb19ecbaeeffcc588c
sakshiagrawal03 <agrawalsag151@gmail.com>
2026-04-09 16:53:29 +0530

    fix: resolve DOMMatrix crash, fix db typo, and optimize for vercel

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
+// require('dotenv').config();
 const app = require('./src/app');
 const connectToDB = require('./src/config/db');
 
 connectToDB();  
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT|| 3000;
 
 app.listen(PORT, () => {
      console.log(`Server is running on port ${PORT}`);
--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -7,7 +7,7 @@ async function connectToDB(){
         await mongoose.connect(process.env.MONGO_URI);
         console.log('Connected to Database');
     } catch (err) {
-        console.err('Error connecting to Database:',err);
+        console.errror('Error connecting to Database:',err);
     }
 }
 
