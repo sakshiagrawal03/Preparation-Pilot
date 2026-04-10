a20f8e171211439c9f41a509c44254854cf5728c
sakshiagrawal03 <agrawalsag151@gmail.com>
2026-04-10 15:29:38 +0530

    final commit

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,17 +1,4 @@
-// const mongoose= require('mongoose');
 
-
-
-// async function connectToDB(){
-//     try{
-//         await mongoose.connect(process.env.MONGO_URI);
-//         console.log('Connected to Database');
-//     } catch (err) {
-//         console.error('Error connecting to Database:',err);
-//     }
-// }
-
-// module.exports= connectToDB;
 const mongoose = require('mongoose');
 
 // We use a global variable to cache the connection in serverless environments
